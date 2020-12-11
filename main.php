efe2ce359e5660b6e07f6e76b75fb774d12fd168
TadasLTU <semionovas13@gmail.com>
2020-12-11 05:19:36 +0200

    Update main.php

--- main.php
+++ main.php
@@ -1,11 +1,19 @@
-<h1 style="text-align: center;">Medžiagų mokslo projektinis darbas</h1>
-<p>&Scaron;io projektinio darbo tikslas integruoti fizikos laboratorijoje atliktą darbą su mūsų studijuojama sritimi</p>
-<p>Darbo &scaron;altinio kodą galite rasti <a title="https://github.com/TadasLTU/KTU-medziagu-mokslo-darbas" href="https://github.com/TadasLTU/KTU-medziagu-mokslo-darbas">čia</a></p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p>Darbą rengė:</p>
-<p>Miglė zubavičiūtė IFB-7 gr.</p>
-<p>Nedas Žilovas IFC-7 gr.</p>
-<p>Erikas Sniečkus IFC-7 gr.</p>
-<p>Tadas Semionovas IFC-7 gr.</p>
-<p>Mantas Valatka IFB-7 gr.</p>
+<div style="text-align: center;">
+    <h1>Medžiagų mokslo projektinis darbas</h1>
+    <p>&Scaron;io projektinio darbo tikslas integruoti fizikos laboratorijoje atliktą darbą su mūsų studijuojama sritimi</p>
+</div>
+<div class="row">
+    <div class="col-sm-2"></div>
+    <div class="col-md-8">
+        <p>Darbo &scaron;altinio kodą galite rasti <a title="https://github.com/TadasLTU/KTU-medziagu-mokslo-darbas" href="https://github.com/TadasLTU/KTU-medziagu-mokslo-darbas">čia</a></p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>Darbą rengė:</p>
+        <p>Miglė zubavičiūtė IFB-7 gr.</p>
+        <p>Nedas Žilovas IFC-7 gr.</p>
+        <p>Erikas Sniečkus IFC-7 gr.</p>
+        <p>Tadas Semionovas IFC-7 gr.</p>
+        <p>Mantas Valatka IFB-7 gr.</p>
+    </div>
+    <div class="col-sm-2"></div>
+</div>
